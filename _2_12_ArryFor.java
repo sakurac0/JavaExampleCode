772a5546cafee469a71b81f381ae007aec46ecfc
sakurac0 <37287316+sakurac0@users.noreply.github.com>
2018-07-19 18:04:32 +0900

    Add files via upload

---
+++ _2_12_ArryFor.java
@@ -0,0 +1,10 @@
+package example;
+
+public class _2_12_ArryFor {
+	public static void main(String[] args) {
+		String arr[] = { "Hello", "Java", "Nice to meet you", "Bye!" }; // 1차원배열
+		for (int i = 0; i < 4; i++) { // 1중 for문
+			System.out.println(arr[i]);
+		}
+	}
+}
