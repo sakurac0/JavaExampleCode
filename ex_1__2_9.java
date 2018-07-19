1ba48f889f3604f422605e0fdade79232ee66a10
sakurac0 <37287316+sakurac0@users.noreply.github.com>
2018-07-19 16:53:20 +0900

    Add files via upload

---
+++ ex_1__2_9.java
@@ -0,0 +1,44 @@
+package example;
+
+public class ex_1__2_9 {
+public static void main(String[] args) {
+System.out.println("Hello! CMS");
+String str1 = "Hello";
+String str2 = "CMS";
+String str3 = "Welcome";
+System.out.println(str1);
+System.out.println(str2);
+System.out.println(str3);
+System.out.println("1+1="+(1+1));
+System.out.println("3-1="+(3-1));
+System.out.println("2*1="+(2*1));
+System.out.println("2/2="+(2/2));
+System.out.println("2/2 나머지 값은 "+(2%2));
+int a = 10;
+
+a += 2;
+System.out.println(a);
+a -= 2;
+System.out.println(a);
+a *= 2;
+System.out.println(a);
+a /= 2;
+System.out.println(a);
+a %= 5;
+System.out.println(a);
+
+System.out.println(++a);
+System.out.println(a--);
+System.out.println(++a);
+
+int b = 2;
+System.out.println("a<b : "+(a<b));
+System.out.println("a>b : "+(a>b));
+System.out.println("a!= : "+(a!=b));
+System.out.println("a==b : "+(a==b));
+
+System.out.println((a==1)&&(b==1));
+System.out.println((a==1)||(b==1));
+System.out.println(!(a==1));
+}
+}
