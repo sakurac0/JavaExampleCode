40f2417796217d63829261dbbea3a40378090ab5
sakurac0 <37287316+sakurac0@users.noreply.github.com>
2018-07-19 15:58:47 +0900

    Add files via upload

---
+++ _2_10Arry.java
@@ -0,0 +1,11 @@
+package example;
+
+public class _2_10Arry {
+public static void main(String[] args) {
+	String str[] = {"Hello","Java","Nice to meet you","Bye!"}; // 1차원배열
+System.out.println(str[0]);
+System.out.println(str[1]);
+System.out.println(str[2]);
+System.out.println(str[3]);
+}
+}
