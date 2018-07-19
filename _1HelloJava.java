7d730e546b02edd7e3d20edbc33b680449960cc3
sakurac0 <37287316+sakurac0@users.noreply.github.com>
2018-07-19 17:25:43 +0900

    Add files via upload

--- _1HelloJava.java
+++ _1HelloJava.java
@@ -4,8 +4,7 @@ public class _1HelloJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome to Java World!"); // Welcome to Java world!
-														// 출력
+		System.out.println("Welcome to Java World!"); // Welcome to Java world! 출력
 	}
 
 }
