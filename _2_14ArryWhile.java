7ab136b4136dde78a694d5c46f06bf2391ced9a3
sakurac0 <37287316+sakurac0@users.noreply.github.com>
2018-07-19 20:14:39 +0900

    Update _2_14ArryWhile.java

--- _2_14ArryWhile.java
+++ _2_14ArryWhile.java
@@ -4,7 +4,7 @@ public class _2_14ArryWhile {
 	public static void main(String[] args) {
 		String str[] = { "Hello", "Java", "Nice", "to", "meet you", "Bye!" };
 		int i = 0;
-		while (i < 5) {
+		while (i < 5) { // i값이 5가 될때까지 반복
 			System.out.println(str[i]);
 			i++;
 		}
