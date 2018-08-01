5e8ef392016831571cb29e5a6db9755b48789763
sakurac0 <37287316+sakurac0@users.noreply.github.com>
2018-08-01 13:20:48 +0900

    Add files via upload

--- _2_10Arry.java
+++ _2_10Arry.java
@@ -4,10 +4,18 @@ public class _2_10Arry {
 	public static void main(String[] args) {
 
 		String str[] = { "Hello", "Java", "Nice to meet you", "Bye!" }; // 1차원배열
-		
+
 		System.out.println(str[0]); // 시스템 콘솔창에 출력하고 한줄 띄운다
 		System.out.println(str[1]);
 		System.out.println(str[2]);
 		System.out.println(str[3]);
+
+		int arr1[] = new int[10], j = 0; // 1차원배열 정수형 변수 arr1[10]을 선언한다
+
+		for (int i = 0; i <= 9; i++) { // arr1[10] 정수형 변수를 이용해 1~10까지 출력한다
+			++j;
+			arr1[i] = arr1[i] + j;
+			System.out.println(arr1[i]);
+		}
 	}
 }
--- _2_11dimensionalArry.java
+++ _2_11dimensionalArry.java
@@ -2,15 +2,26 @@ package example;
 
 public class _2_11dimensionalArry {
 	public static void main(String[] args) {
-		
+
 		int arr2[][] = { { 1, 2, 3 }, { 4, 5, 6 } }; // 2차원배열
-		
-		System.out.println("arr2[0][0] = " + arr2[0][0]); // 시스템 콘솔창에 출력하고 한줄 띄운다
+
+		System.out.println("arr2[0][0] = " + arr2[0][0]); // 시스템 콘솔창에 출력하고 한줄
+															// 띄운다
 		System.out.println("arr2[0][1] = " + arr2[0][1]);
 		System.out.println("arr2[0][2] = " + arr2[0][2]);
 		System.out.println("arr2[1][0] = " + arr2[1][0]);
 		System.out.println("arr2[1][1] = " + arr2[1][1]);
 		System.out.println("arr2[1][2] = " + arr2[1][2]);
+
+		int arr3[][] = new int[2][5], k = 0; // 2차원배열 정수형변수 arr3[2][5]를 선언한다
+
+		for (int i = 0; i <= 1; i++) { // arr3[2][5] 정수형 변수를 이용해 1~10까지 출력한다
+			for (int j = 0; j <= 4; j++) {
+				++k;
+				arr3[i][j] = arr3[i][j] + k;
+				System.out.println(arr3[i][j]);
+			}
+		}
 
 	}
 }
