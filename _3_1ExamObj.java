a8e5128347a7b4bf55f6203ce904ec87d9002713
sakurac0 <37287316+sakurac0@users.noreply.github.com>
2018-08-03 16:25:56 +0900

    Add files via upload

--- _3_1ExamObj.java
+++ _3_1ExamObj.java
@@ -2,10 +2,13 @@ package example;
 
 class _3_1ExamObj {
 	String a;
+	// 문자열을 담는 a라는 변수를 선언한다
 	int b;
 
+	// 정수값을 담는 a라는 변수를 선언한다
 	String c() {
 		return "ExamObjReturnTest";
+		// 문자열을 리턴값으로 받는 c라는 메서드를 선언한다
 	}
 
 }
--- _3_2ExampleObj.java
+++ _3_2ExampleObj.java
@@ -1,11 +1,13 @@
 package example;
 
-class _3_2ExampleObj {
+class _3_2ExampleObj { // 객체를정의 생성하는툴인 _3_2ExampleObj이라는 변수를 선언한다
 	public static void main(String[] args) {
-		_3_1ExamObj ExamObj1;
-		ExamObj1 = new _3_1ExamObj();
+		_3_1ExamObj ExamObj1; // _3_1ExamObj이라는 객체를 담는 ExamObj1이라는 변수를 선언한다
+		ExamObj1 = new _3_1ExamObj();  
 		ExamObj1.a = "ExamObjTestOK";
+		// ExamObj1변수안의 a변수에 "ExamObjTestOK"을 저장한다
 		ExamObj1.b = 1234;
+// ExamObj1변수안의 b변수에 "1234"을 저장한다
 		System.out.println(ExamObj1.a);
 		System.out.println(ExamObj1.b);
 		System.out.println(ExamObj1.c());
