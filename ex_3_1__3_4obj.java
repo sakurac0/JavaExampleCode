7764d5f5dfe31c0e911f438e0170911a45a23273
sakurac0 <37287316+sakurac0@users.noreply.github.com>
2018-08-06 17:25:02 +0900

    Add files via upload

---
+++ ex_3_1__3_4obj.java
@@ -0,0 +1,16 @@
+package example;
+
+class ex_3_1__3_4obj {
+	public static void main(String[] args) {
+
+		ex_3_1__3_4 exobj = new ex_3_1__3_4();
+
+		exobj.age = 36;
+		exobj.weight = 61.0;
+		exobj.name = "cms";
+		System.out.println(exobj.age);
+		System.out.println(exobj.weight);
+		System.out.println(exobj.name);
+		System.out.println(exobj.hello());
+	}
+}
