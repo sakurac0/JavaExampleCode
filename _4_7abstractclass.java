a85cfdbfb218a117ccaa13029dbd17d6e7955f0b
sakurac0 <37287316+sakurac0@users.noreply.github.com>
2018-08-14 17:40:50 +0900

    Add files via upload

--- _4_7abstractclass.java
+++ _4_7abstractclass.java
@@ -1,6 +1,6 @@
 package example;
 
-abstract class _4_7abstractclass {
+public abstract class _4_7abstractclass {
 	// 구체적인 내용없이 공통점만 뽑아낸, 객체를 정의 하는 툴인 _4_7abstractclass이라는 클래스를 만든다
 	abstract void cry();
 	// 구체적인 내용 없이 공통점만 뽑아낸, 리턴값이 없는 cry라는 메서드를 선언한다
---
+++ _4_9NonPolymorphismTest.java
@@ -0,0 +1,16 @@
+package example;
+
+public class _4_9NonPolymorphismTest {
+
+	public static void main(String[] args) {
+	
+		_4_7abstractclassEctenedsDog Dog = new _4_7abstractclassEctenedsDog();
+		_4_7abstractclassEctenedsCat Cat = new _4_7abstractclassEctenedsCat();
+		
+		Dog.cry();
+		Cat.cry();
+		
+	
+	}
+
+}
