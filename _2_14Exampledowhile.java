c4c1f943508971eea2cbebc6d945d79e71fe4cec
sakurac0 <37287316+sakurac0@users.noreply.github.com>
2018-08-22 19:49:09 +0900

    Add files via upload

---
+++ _2_14Exampledowhile.java
@@ -0,0 +1,13 @@
+package example;
+
+public class _2_14Exampledowhile {
+	public static void main(String[] args) {
+
+		int i = 1;
+		do {
+		
+			System.out.println("DowhileTest"); 
+		
+		} while (i != 1); // 조건식이 false이더라도 한번은 시행
+	}
+}
